88f5c31a63526d68f0dcb482d7093ee38ee0365e
Tuller <j85ason+git@gmail.com>
2010-06-08 23:10:46 -0400

    Moved more settings from constants to getters

--- OmniCC/core.lua
+++ OmniCC/core.lua
@@ -1,18 +1,12 @@
 --[[
 	OmniCC
-		A featureless, 'pure' version of OmniCC.
-		This version should work on absolutely everything, but I've removed pretty much all of the options
+		Displays text for cooldowns
 --]]
 
---[[ constants & local bindings ]]--
-
+--constants!
 local PADDING = 2
-local ICON_SIZE = 36 --the normal size for an icon (don't change this)
-local TEXT_FONT = STANDARD_TEXT_FONT --what font to use
-local FONT_SIZE = 18 --the base font size to use at a scale of 1
-local MIN_SCALE = 0.8 --the minimum scale we want to show cooldown counts at, anything below this will be hidden
-local MIN_DURATION = 3 --the minimum duration to show cooldown text for
-local DAY, HOUR, MINUTE = 86400, 3600, 60
+local ICON_SIZE = 36 --the normal size for an icon
+local DAY, HOUR, MINUTE = 86400, 3600, 60 --value in seconds for days, hours, and minutes
 local UPDATE_DELAY = 0.01 --minimum time between timer updates
 
 --omg speed
@@ -20,24 +14,10 @@ local format = string.format
 local floor = math.floor
 local min = math.min
 
+
 --[[
 	Timer Code
 --]]
-
-local function formatTime(s)
-	if s >= DAY then
-		return GRAY_FONT_COLOR_CODE .. format('%dd', floor(s/DAY + 0.5)) .. FONT_COLOR_CODE_CLOSE
-	elseif s >= HOUR then
-		return NORMAL_FONT_COLOR_CODE .. format('%dh', floor(s/HOUR + 0.5)) .. FONT_COLOR_CODE_CLOSE
-	elseif s >= MINUTE then
-		return YELLOW_FONT_COLOR_CODE .. format('%dm', floor(s/MINUTE + 0.5)) .. FONT_COLOR_CODE_CLOSE
-	elseif floor(s + 0.5) > 10 then
-		return YELLOW_FONT_COLOR_CODE .. floor(s + 0.5) .. FONT_COLOR_CODE_CLOSE
-	elseif s >= 3 then
-		return RED_FONT_COLOR_CODE .. floor(s + 0.5) .. FONT_COLOR_CODE_CLOSE
-	end
-	return GREEN_FONT_COLOR_CODE .. format('%.1f', s) .. FONT_COLOR_CODE_CLOSE
-end
 
 local Timer = CreateFrame('Frame'); Timer:Hide()
 local timer_MT = {__index = Timer}
@@ -51,7 +31,7 @@ function Timer:New(parent)
 
 	local text = t:CreateFontString(nil, 'OVERLAY')
 	text:SetPoint('CENTER', 0, 1)
-	text:SetFont(TEXT_FONT, FONT_SIZE, 'OUTLINE')
+	text:SetFont(OmniCC:GetFont())
 	t.text = text
 
 	return t
@@ -92,25 +72,25 @@ function Timer:Update()
 		self:UpdateDisplay()
 	else
 		self:Stop()
-		OmniCC:SendMessage('COOLDOWN_FINISHED', self)
+		OmniCC:SendMessage('COOLDOWN_FINISHED', self:GetParent())
 	end
 end
 
 function Timer:UpdateDisplay()
-	local rScale = self:GetEffectiveScale() / UIParent:GetEffectiveScale()
-	local iconScale = floor(self:GetWidth() - PADDING + 0.5) / ICON_SIZE --icon sizes seem to vary a little bit, so this takes care of making them round to whole numbers
+	local scale = floor(self:GetWidth() - PADDING + 0.5) / ICON_SIZE --icon sizes seem to vary a little bit, so this takes care of making them round to whole numbers
+	local font, size, outline = OmniCC:GetFont(scale)
 	local text = self.text
 
-	if (iconScale*rScale) < MIN_SCALE or iconScale <= 0 then
+	if fontSize < OmniCC:GetMinFontSize() then
 		text:Hide()
 	else
-		local fontSize = FONT_SIZE * iconScale
-		if not(text.fontSize and text.fontSize == fontSize) then
-			text:SetFont(TEXT_FONT, fontSize, 'OUTLINE')
-			text.fontSize = fontSize
+		if not(text.font == font and text.fontSize == size and text.fontOutline == outline) then
+			text:SetFont(font, size, outline)
+			text.font = font
+			text.fontSize = size
+			text.fontOutline = outline
 		end
-
-		text:SetText(formatTime(self:GetRemainingTime()))
+		text:SetText(OmniCC:GetFormattedTime(self:GetRemainingTime()))
 		text:Show()
 	end
 end
@@ -119,14 +99,14 @@ function Timer:GetRemainingTime()
 	return self.duration - (GetTime() - self.start)
 end
 
+
 --[[
-	global updater
+	Global Updater/Event Handler
 --]]
 
 local OmniCC = CreateFrame('Frame', 'OmniCC', UIParent); OmniCC:Hide()
-OmniCC:RegisterEvent('PLAYER_ENTERING_WORLD')
 OmniCC.timers = {}
-OmniCC.nextUpdate = 0
+OmniCC.elapsed = UPDATE_DELAY
 
 OmniCC:SetScript('OnEvent', function(self, event, ...)
 	local a = self[event]
@@ -135,22 +115,23 @@ OmniCC:SetScript('OnEvent', function(self, event, ...)
 	end
 end)
 
-OmniCC:SetScript('OnHide', function(self, elapsed)
-	self.nextUpdate = 0
-end)
-
 OmniCC:SetScript('OnUpdate', function(self, elapsed)
 	if not next(self.timers) then
 		self:Hide()
 		return
 	end
 
-	if self.nextUpdate > 0 then
-		self.nextUpdate = self.nextUpdate - elapsed
+	if self.elapsed < UPDATE_DELAY then
+		self.elapsed = self.elapsed + elapsed
 	else
-		self.nextUpdate = UPDATE_DELAY
+		self.elapsed = 0
 		self:UpdateTimers()
 	end
+end)
+
+
+OmniCC:SetScript('OnHide', function(self, elapsed)
+	self.elapsed = UPDATE_DELAY
 end)
 
 
@@ -162,6 +143,8 @@ end)
 function OmniCC:PLAYER_ENTERING_WORLD()
 	self:UpdateTimers()
 end
+
+OmniCC:RegisterEvent('PLAYER_ENTERING_WORLD')
 
 
 --[[
@@ -195,15 +178,41 @@ function OmniCC:UpdateTimers()
 end
 
 --[[
-	the hook
+	Config Settings
 --]]
 
---ActionButton1Cooldown here, is something we think will always exist
-local methods = getmetatable(_G['ActionButton1Cooldown']).__index
-hooksecurefunc(methods, 'SetCooldown', function(self, start, duration)
-	if start > 0 and duration > MIN_DURATION then
-		Timer:Start(self, start, duration)
-	elseif self.timer then
-		self.timer:Stop()
+--how many seconds, in length, must a cooldown be to show text
+function OmniCC:GetMinDuration()
+	return 3
+end
+
+--enables|disables showing cooldown models
+function OmniCC:ShowingModels()
+	return true
+end
+
+--retrieves font information at the given scale
+--defaults scale to 1 if omitted
+function OmniCC:GetFont(scale)
+	return STANDARD_TEXT_FONT, 18 * (scale or 1), 'OUTLINE'
+end
+
+--returns the minimum size to display text at
+function OmniCC:GetMinFontSize()
+	return 8
+end
+
+function OmniCC:GetFormattedTime(s)
+	if s >= DAY then
+		return GRAY_FONT_COLOR_CODE .. format('%dd', floor(s/DAY + 0.5)) .. FONT_COLOR_CODE_CLOSE
+	elseif s >= HOUR then
+		return NORMAL_FONT_COLOR_CODE .. format('%dh', floor(s/HOUR + 0.5)) .. FONT_COLOR_CODE_CLOSE
+	elseif s >= MINUTE then
+		return YELLOW_FONT_COLOR_CODE .. format('%dm', floor(s/MINUTE + 0.5)) .. FONT_COLOR_CODE_CLOSE
+	elseif floor(s + 0.5) > 10 then
+		return YELLOW_FONT_COLOR_CODE .. floor(s + 0.5) .. FONT_COLOR_CODE_CLOSE
+	elseif s >= 3 then
+		return RED_FONT_COLOR_CODE .. floor(s + 0.5) .. FONT_COLOR_CODE_CLOSE
 	end
-end)
+	return GREEN_FONT_COLOR_CODE .. format('%.1f', s) .. FONT_COLOR_CODE_CLOSE
+end
