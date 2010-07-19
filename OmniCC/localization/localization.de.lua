3fb5d0a3bfe84dae319c76cd8f89676017a9e770
Tuller <j85ason+git@gmail.com>
2010-07-19 19:58:21 -0400

    Updated localization (really)

---
+++ OmniCC/localization/localization.de.lua
@@ -0,0 +1,12 @@
+--[[
+	OmniCC localization - deDE
+--]]
+
+if GetLocale() ~= 'deDE' then return end
+
+local L = OMNICC_LOCALS
+
+L.Updated = "Aktualisiert auf v%s"
+L.None = NONE
+L.Pulse = "Pulse"
+L.Shine = "Shine"
---
+++ OmniCC_Config/localization/localization.de.lua
@@ -0,0 +1,47 @@
+--[[
+	OmniCC configuration interface localization - deDE
+--]]
+
+if GetLocale() ~= 'deDE' then return end
+
+local L = OMNICC_LOCALS
+
+L.GeneralSettings = "Allgemein"
+L.GeneralSettingsTitle = "Die allgemeinen Sachen"
+
+L.FontSettings = "Schriftart"
+L.FontSettingsTitle = "Gib den Cooldowns neues Leben"
+
+L.BlacklistSettings = "Schwarze Liste"
+L.BlacklistSettingsTitle = "Hey! Keinen Text dort anzeigen!"
+
+L.UseWhitelist = "Zeige Text nur auf registrierten Frames"
+L.UseBlacklist = "Zeige Text nur auf Frames, die nicht auf der Schwarzen Liste sind"
+L.Blacklist = "Schwarze Liste"
+
+L.Font = "Schrift"
+L.FontSize = "Basis Schriftgr\195\182\195\159e"
+L.FontOutline = "Schriftkontur"
+
+L.Outline_NONE = NONE
+L.Outline_OUTLINE = "D\195\188nn"
+L.Outline_THICKOUTLINE = "Dick"
+
+L.MinDuration = "Kleinste Dauer um Text anzuzeigen"
+L.MinFontSize = "Kleinste Schriftgr\195\182\195\159e um Text anzuzeigen"
+L.ScaleText = "Skaliere Text innerhalb Frames automatisch"
+
+L.Add = "Hinzuf\195\188gen"
+L.Remove = "Entfernen"
+
+L.FinishEffect = 'Abschlusseffekt'
+L.MinEffectDuration = 'Kleinste Dauer um Abschlusseffekt anzuzeigen'
+
+L.MMSSDuration = "Kleinste Dauer um Text als MM:SS anzuzeigen"
+L.TenthsDuration = "Kleinste Dauer f\195\188r Millisekunden im Text"
+
+L.Color = "Farbe"
+L.Color_soon = "L\195\164uft bald ab"
+L.Color_seconds = "Unter einer Minute"
+L.Color_minutes = "Unter einer Stunde"
+L.Color_hours = "Eine Stunde und mehr"
