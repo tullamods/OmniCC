2c3ff7ba12fa618795b0ac2ea6277db0a4c429c2
Tuller <j85ason+git@gmail.com>
2010-09-22 18:28:45 -0400

    updated localization, fixed tooltip bugs

--- OmniCC_Config/localization/localization.de.lua
+++ OmniCC_Config/localization/localization.de.lua
@@ -1,4 +1,4 @@
---[[
+﻿--[[
 	OmniCC configuration interface localization - deDE
 --]]
 
@@ -12,8 +12,8 @@ L.GeneralSettingsTitle = "Die allgemeinen Sachen"
 L.FontSettings = "Schriftart"
 L.FontSettingsTitle = "Gib den Cooldowns neues Leben"
 
-L.BlacklistSettings = "Schwarze Liste"
-L.BlacklistSettingsTitle = "Hey! Keinen Text dort anzeigen!"
+L.FilterSettings = "Filter"
+L.FilterSettingsTitle = "Hey! Keinen Text dort anzeigen!"
 
 L.UseWhitelist = "Zeige Text nur auf registrierten Frames"
 L.UseBlacklist = "Zeige Text nur auf Frames, die nicht auf der Schwarzen Liste sind"
@@ -29,7 +29,7 @@ L.Outline_THICKOUTLINE = "Dick"
 
 L.MinDuration = "Kleinste Dauer um Text anzuzeigen"
 L.MinFontSize = "Kleinste Schriftgr\195\182\195\159e um Text anzuzeigen"
-L.ScaleText = "Skaliere Text innerhalb Frames automatisch"
+L.ScaleText = "Text automatisch an Frames anpassen"
 
 L.Add = "Hinzuf\195\188gen"
 L.Remove = "Entfernen"
@@ -38,10 +38,73 @@ L.FinishEffect = 'Abschlusseffekt'
 L.MinEffectDuration = 'Kleinste Dauer um Abschlusseffekt anzuzeigen'
 
 L.MMSSDuration = "Kleinste Dauer um Text als MM:SS anzuzeigen"
-L.TenthsDuration = "Kleinste Dauer f\195\188r Millisekunden im Text"
+L.TenthsDuration = "Kleinste Dauer f\195\188r Zehntelsekunden im Text"
 
-L.Color = "Farbe"
+L.ColorAndScale = "Farbe und Gr\195\182\195\159e"
 L.Color_soon = "L\195\164uft bald ab"
 L.Color_seconds = "Unter einer Minute"
 L.Color_minutes = "Unter einer Stunde"
 L.Color_hours = "Eine Stunde und mehr"
+
+L.ShowCooldownModels = "Zeige Cooldown Spirale"
+
+L.Sec = " Sek."
+
+--[[ Tooltips ]]--
+
+L.ScaleTextTip =
+[[Verkleinert den Text, so
+dass er auch in Frames passt,
+die zu klein sind.]]
+
+L.ShowCooldownModelsTip =
+[[Zeigt die dunkle Spirale,
+die man normalerweise sieht,
+wenn etwas Cooldown hat.]]
+
+L.MinDurationTip =
+[[Bestimmt, wie lang ein Cooldown
+sein muss, damit er angezeigt wird.
+
+Das wird hauptsächlich verwendet,
+um den GCD auszublenden.]]
+
+L.MinFontSizeTip =
+[[Bestimmt, wie groß der Cooldown-
+Text mindestens sein muss, damit er
+angezeigt wird.
+
+Das wird hauptsächlich verwendet,
+um unlesbaren Text auszublenden, und um
+keinen Text auf Buffs anzuzeigen.]]
+
+L.MinEffectDurationTip =
+[[Bestimmt, wie lang ein Cooldown
+sein muss, damit ein Abschlusseffekt
+angezeigt wird (Pulse/Shine).]]
+
+L.MMSSDurationTip =
+[[Bestimmt, bis zu welcher verbleibenden
+Dauer ein Cooldown im MM:SS Format
+angezeigt wird.]]
+
+L.TenthsDurationTip =
+[[Bestimmt, ab wann Zehntelsekunden
+angezeigt werden.]]
+
+L.FontSizeTip =
+[[Bestimmt, wie groß der Text ist.]]
+
+L.FontOutlineTip =
+[[Bestimmt die Dicke der Kontur um den Text.]]
+
+L.UseBlacklistTip =
+[[Aktiviert die Schwarze Liste.
+Wenn aktiv, werden alle Frames, deren
+Name in der Schwarzen Liste vorkommt,
+keinen Cooldown-Text anzeigen.]]
+
+L.FrameStackTip =
+[[Aktiviert bzw. deaktiviert die Anzeige
+der Namen von Frames, wenn die Maus
+darüber ist.]]
--- OmniCC_Config/localization/localization.lua
+++ OmniCC_Config/localization/localization.lua
@@ -33,7 +33,7 @@ L.Add = "Add"
 L.Remove = "Remove"
 
 L.FinishEffect = "Finish Effect"
-L.MinEffectDuration = "Minimum duration to display finish effect"
+L.MinEffectDuration = "Minimum duration to display a finish effect"
 
 L.MMSSDuration = "Minimum duration to display text as MM:SS"
 L.TenthsDuration = "Minimum duration to display tenths of seconds"
@@ -51,62 +51,62 @@ L.Sec = "Sec"
 --[[ Tooltips ]]--
 
 L.ScaleTextTip =
-	[[When enabled, this setting
-	will cause text to shrink to
-	fit within frames that are
-	too small]]
+[[When enabled, this setting
+will cause text to shrink to
+fit within frames that are
+too small]]
 
 L.ShowCooldownModelsTip =
-	[[Controls the display of cooldown
-	models (the dark spiral you normally
-	see on a button when on cooldown]]
+[[Controls the display of cooldown
+models (the dark spiral you normally
+see on a button when on cooldown]]
 
 L.MinDurationTip =
-	[[Determines how long a cooldown
-	must be in order to show text.
+[[Determines how long a cooldown
+must be in order to show text.
 
-	This setting is mainly used to
-	filter out the GCD]]
+This setting is mainly used to
+filter out the GCD]]
 
 L.MinFontSizeTip =
-	[[Determines how big the overall
-	font size of a cooldown text object
-	must be in order to display text.
-	The larger the value, the larger a
-	button must be.
+[[Determines how big the overall
+font size of a cooldown text object
+must be in order to display text.
+The larger the value, the larger a
+button must be.
 
-	This setting is mainly used to filter
-	out unreadable text, as well as filter
-	out text from displaying on buffs]]
+This setting is mainly used to filter
+out unreadable text, as well as filter
+out text from displaying on buffs]]
 
 L.MinEffectDurationTip =
-	[[Determines how long a
-	cooldown must be in order
-	to show a finish effect
-	(ex, pulse/shine)]]
+[[Determines how long a
+cooldown must be in order
+to show a finish effect
+(ex, pulse/shine)]]
 
 L.MMSSDurationTip =
-	[[Determines the threshold
-	for showing a cooldown
-	in a MM:SS format]]
+[[Determines the threshold
+for showing a cooldown
+in a MM:SS format]]
 
 L.TenthsDurationTip =
-	[[Determines the threshold
-	for showing tenths of seconds]]
+[[Determines the threshold
+for showing tenths of seconds]]
 
 L.FontSizeTip =
-	[[Controls how large text is]]
+[[Controls how large text is]]
 
 L.FontOutlineTip =
-	[[Controls the thickness of the outline around text]]
+[[Controls the thickness of the outline around text]]
 
 L.UseBlacklistTip =
-	[[Click this to toggle using the blacklist.
-	When enabled, any frame with a name
-	that matches an item on the blacklist
-	will not display cooldown text.]]
+[[Click this to toggle using the blacklist.
+When enabled, any frame with a name
+that matches an item on the blacklist
+will not display cooldown text.]]
 
 L.FrameStackTip =
-	[[Toggles showing the names
-	of frames when you hover
-	over them]]
+[[Toggles showing the names
+of frames when you hover
+over them]]
