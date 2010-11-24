dfe092ebeea6f9df1f9541b2a6e45e0147d0f3b3
狂飙 <zzjbcn@gmail.com>
2010-11-24 15:26:11 +0800

    zhCN and zhTW localization files update

--- localization/localization.cn.lua
+++ localization/localization.cn.lua
@@ -1,13 +1,13 @@
 --[[
-	OmniCC configuration interface localization - zhCN
+	OmniCC configuration interface localization - Chinese Simplified
 --]]
 
 if GetLocale() ~= 'zhCN' then return end
 
 local L = OMNICC_LOCALS
 
-L.GeneralSettings = "一般"
-L.FontSettings = "设定字形"
+L.GeneralSettings = "显示"
+L.FontSettings = "文字风格"
 L.RuleSettings = "规则"
 L.PositionSettings = "文字位置"
 
@@ -19,29 +19,29 @@ L.PositionSettings = "文字位置"
 --L.UseBlacklist = "只在框格但不在黑名单上显示文字"
 --L.Blacklist = "黑名单"
 
-L.Font = "字形"
-L.FontSize = "字形大小"
-L.FontOutline = "字形轮廓"
+L.Font = "字体"
+L.FontSize = "默认字体大小"
+L.FontOutline = "字体轮廓"
 
 L.Outline_NONE = NONE
-L.Outline_OUTLINE = "幼线"
+L.Outline_OUTLINE = "细线"
 L.Outline_THICKOUTLINE = "粗线"
 
-L.MinDuration = "最少时间的时值"
-L.MinSize = "最小的字体大小来显示文字"
-L.ScaleText = "使用自动缩放令文字保持在框格之内"
+L.MinDuration = "显示文字的最少时间"
+L.MinSize = "显示文字的最小字号"
+L.ScaleText = "使用自动缩放令文字保持在框架之内"
 L.EnableText = "启用冷却文字"
 
 L.Add = "新增"
 L.Remove = "移除"
 
-L.FinishEffect = "完成的效果"
-L.MinEffectDuration = "完成的效果最少时间的时值"
+L.FinishEffect = "完成效果"
+L.MinEffectDuration = "完成效果的最少时间"
 
-L.MMSSDuration = "最少时间的时值以 MM:SS 格式来显示"
+L.MMSSDuration = "最少时间的文字以 MM:SS 格式来显示"
 L.TenthsDuration = "以十分之一秒为单位显示"
 
-L.ColorAndScale = "颜色 & 比例"
+L.ColorAndScale = "颜色 & 缩放"
 L.Color_soon = "即将到期"
 L.Color_seconds = "少于1分钟"
 L.Color_minutes = "少于1小时"
@@ -64,52 +64,52 @@ L.Anchor_BOTTOMLEFT = '左下'
 L.Anchor_BOTTOM = '下'
 L.Anchor_BOTTOMRIGHT = '右下'
 
---群组
-L.Groups = '群组'
-L.Group_base = '预设'
+--分组
+L.Groups = '分组'
+L.Group_base = '默认'
 L.Group_action = '动作'
 L.Group_aura = '光环'
 L.Group_pet = '宠物动作'
-L.AddGroup = '新增群组...'
+L.AddGroup = '新增分组...'
 
---[[ 提示贴]]--
+--[[ 提示 ]]--
 
 L.ScaleTextTip =
 [[当启用时，此设置 
 会令文字缩小来适应
-太细小的框架]]
+太小的框架]]
 
 L.ShowCooldownModelsTip =
-[[控制冷却模组的显示方式
+[[控制冷却时间的显示模式
 （你通常在冷却的按钮看到
-的黑暗螺旋]]
+的黑暗螺旋）]]
 
 L.MinDurationTip =
-[[确定多长的冷却时间才显示文字. 
+[[确定多长的冷却时间才显示文字
 此设置主要用于筛选出GCD]]
 
 L.MinSizeTip =
-[[确定多大的框架能显示文字. 
-该值越小,可以展示的东西越小. 
-值越大, 可以展示的东西越大.
+[[确定多大的框架能显示文字。
+该值越小，可以展示的东西越小。
+该值越大，可以展示的东西越大。
 
 一些基准:
 100 - 动作按钮的大小
 80  - 职业或宠物动作按钮的大小
-55  - 暴雪目标增益框的大小]]
+55  - 暴雪目标增益框架的大小]]
 
 L.MinEffectDurationTip =
-[[确定须要多长的冷却
-时间来显示一个完成的效果 
-(例如, 脉冲 /闪亮)]]
+[[确定需要多长的冷却
+时间来显示一个完成的效果
+（例如，脉冲/闪亮）]]
 
 L.MMSSDurationTip =
-[[确定用于显示冷却时间的界限点
+[[确定用于显示冷却时间的阈值
  以 MM:SS 格式来显示]]
 
 L.TenthsDurationTip =
-[[确定用于显示冷却时间的界限点
-以十份一秒格式来显示]]
+[[确定用于显示冷却时间的阈值
+以十分之一秒格式来显示]]
 
 L.FontSizeTip =
 [[控制文字的大小]]
@@ -118,10 +118,10 @@ L.FontOutlineTip =
 [[控制文字周围的轮廓厚度]]
 
 L.UseBlacklistTip =
-[[点击此切换使用黑名单. 
-当启用时，任何框架的名称 
-与黑名单上的项目相同时 
-将不会显示冷却时间.]]
+[[点击切换使用黑名单。
+当启用时，任何框架的名称
+与黑名单上的项目相同时
+将不会显示冷却时间。]]
 
 L.FrameStackTip =
 [[切换当鼠标悬停
--- localization/localization.tw.lua
+++ localization/localization.tw.lua
@@ -1,13 +1,13 @@
 --[[
-	OmniCC configuration interface localization - zhTW
+	OmniCC configuration interface localization - Chinese Traditional
 --]]
 
 if GetLocale() ~= 'zhTW' then return end
 
 local L = OMNICC_LOCALS
 
-L.GeneralSettings = "一般"
-L.FontSettings = "設定字形"
+L.GeneralSettings = "顯示"
+L.FontSettings = "文字風格"
 L.RuleSettings = "規則"
 L.PositionSettings = "文字位置"
 
@@ -19,31 +19,31 @@ L.PositionSettings = "文字位置"
 --L.UseBlacklist = "只在框格但不在黑名單上顯示文字"
 --L.Blacklist = "黑名單"
 
-L.Font = "字形"
-L.FontSize = "字形大小"
-L.FontOutline = "字形輪廓"
+L.Font = "字體"
+L.FontSize = "預設字體大小"
+L.FontOutline = "字體輪廓"
 
 L.Outline_NONE = NONE
-L.Outline_OUTLINE = "幼線"
+L.Outline_OUTLINE = "細線"
 L.Outline_THICKOUTLINE = "粗線"
 
-L.MinDuration = "最少時間的時值"
-L.MinSize = "最小的字體大小來顯示文字"
-L.ScaleText = "使用自動縮放令文字保持在框格之內"
+L.MinDuration = "顯示文字的最少時間"
+L.MinSize = "顯示文字的最小字型大小"
+L.ScaleText = "使用自動縮放令文字保持在框架之內"
 L.EnableText = "啟用冷卻文字"
 
 L.Add = "新增"
 L.Remove = "移除"
 
-L.FinishEffect = "完成的效果"
-L.MinEffectDuration = "完成的效果最少時間的時值"
+L.FinishEffect = "完成效果"
+L.MinEffectDuration = "完成效果的最少時間"
 
-L.MMSSDuration = "最少時間的時值以 MM:SS 格式來顯示"
+L.MMSSDuration = "最少時間的文字以 MM:SS 格式來顯示"
 L.TenthsDuration = "以十分之一秒為單位顯示"
 
-L.ColorAndScale = "顏色 & 比例"
+L.ColorAndScale = "顏色 & 縮放"
 L.Color_soon = "即將到期"
-L.Color_seconds = "少於1分鍾"
+L.Color_seconds = "少於1分鐘"
 L.Color_minutes = "少於1小時"
 L.Color_hours = "1小時或以上"
 
@@ -64,52 +64,52 @@ L.Anchor_BOTTOMLEFT = '左下'
 L.Anchor_BOTTOM = '下'
 L.Anchor_BOTTOMRIGHT = '右下'
 
---群組
-L.Groups = '群組'
-L.Group_base = '預設'
+--分組
+L.Groups = '分組'
+L.Group_base = '默認'
 L.Group_action = '動作'
 L.Group_aura = '光環'
 L.Group_pet = '寵物動作'
-L.AddGroup = '新增群組...'
+L.AddGroup = '新增分組...'
 
---[[ 提示貼]]--
+--[[ 提示 ]]--
 
 L.ScaleTextTip =
 [[當啟用時，此設置 
 會令文字縮小來適應
-太細小的框架]]
+太小的框架]]
 
 L.ShowCooldownModelsTip =
-[[控制冷卻模組的顯示方式
+[[控制冷卻時間的顯示模式
 （你通常在冷卻的按鈕看到
-的黑暗螺旋]]
+的黑暗螺旋）]]
 
 L.MinDurationTip =
-[[確定多長的冷卻時間才顯示文字. 
+[[確定多長的冷卻時間才顯示文字
 此設置主要用於篩選出GCD]]
 
 L.MinSizeTip =
-[[確定多大的框架能顯示文字. 
-該值越小,可以展示的東西越小. 
-值越大, 可以展示的東西越大.
+[[確定多大的框架能顯示文字。
+該值越小，可以展示的東西越小。
+該值越大，可以展示的東西越大。
 
 一些基準:
 100 - 動作按鈕的大小
 80  - 職業或寵物動作按鈕的大小
-55  - 暴雪目標增益框的大小]]
+55  - 暴雪目標增益框架的大小]]
 
 L.MinEffectDurationTip =
-[[確定須要多長的冷卻
-時間來顯示一個完成的效果 
-(例如, 脈衝 /閃亮)]]
+[[確定需要多長的冷卻
+時間來顯示一個完成的效果
+（例如，脈衝/閃亮）]]
 
 L.MMSSDurationTip =
-[[確定用於顯示冷卻時間的界限點
+[[確定用於顯示冷卻時間的閾值
  以 MM:SS 格式來顯示]]
 
 L.TenthsDurationTip =
-[[確定用於顯示冷卻時間的界限點
-以十份一秒格式來顯示]]
+[[確定用於顯示冷卻時間的閾值
+以十分之一秒格式來顯示]]
 
 L.FontSizeTip =
 [[控制文字的大小]]
@@ -118,17 +118,17 @@ L.FontOutlineTip =
 [[控制文字周圍的輪廓厚度]]
 
 L.UseBlacklistTip =
-[[點擊此切換使用黑名單. 
-當啟用時，任何框架的名稱 
-與黑名單上的項目相同時 
-將不會顯示冷卻時間.]]
+[[點擊切換使用黑名單。
+當啟用時，任何框架的名稱
+與黑名單上的項目相同時
+將不會顯示冷卻時間。]]
 
 L.FrameStackTip =
-[[切換當鼠標懸停
+[[切換當滑鼠懸停
 在框架時顯示的名稱]]
 
 L.XOffsetTip =
-[[控制文字的水平偏移]]
+[[控制文字的水準偏移]]
 
 L.YOffsetTip =
 [[控制文字的垂直偏移]]
