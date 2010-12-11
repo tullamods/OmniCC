954d6ee96432b030680a3c543118353abb10e4b0
unknown <Jason@.(none)>
2010-12-11 14:05:07 -0500

    added Russian localizaton

---
+++ localization/localization.ru.lua
@@ -0,0 +1,17 @@
+--[[
+	OmniCC localization - Russian
+--]]
+
+if GetLocale() ~= 'ruRU' then return end
+
+local L = OMNICC_LOCALS
+--[[
+if not GetLocale() == 'enUS' then
+	return
+end
+--]]
+
+L.Updated = "Обновлено до v%s"
+L.None = NONE
+L.Pulse = "Импульс"
+L.Shine = "Блеск"
