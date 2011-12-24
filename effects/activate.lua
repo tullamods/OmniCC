ff2313d1ed76b165695cd1cefb6c9f037addf4c6
Jaliborc <contact@jaliborc.com>
2011-12-24 16:38:50 +0000

    Closes #28

--- effects/activate.lua
+++ effects/activate.lua
@@ -8,28 +8,31 @@ local L = OMNICC_LOCALS
 
 local hooked = {}
 local active = {}
+local function OnFinish(anim)
+	local overlay = anim:GetParent()
+	local button = overlay:GetParent()
+	
+    if active[button] then
+      ActionButton_HideOverlayGlow(button)
+      active[button] = nil
+    end
+end
 
 OmniCC:RegisterEffect({
   id = 'activate',
   name = L.Activate,
   desc = L.ActivateTip,
   Run = function(self, cooldown)
-    local button = cooldown:GetParent()
-    if button then
-      ActionButton_ShowOverlayGlow(button)
-      active[button] = true
+	local button = cooldown:GetParent()
+	if button then
+		ActionButton_ShowOverlayGlow(button)
+		active[button] = true
 
-      local overlay = button.overlay
-      if not hooked[overlay] then
-        overlay.animIn:HookScript('OnFinished', function()
-          if active[button] then
-            ActionButton_HideOverlayGlow(button)
-            active[button] = nil
-          end
-        end)
-        
-        hooked[overlay] = true
-      end
+		local overlay = button.overlay
+		if not hooked[overlay] then
+			overlay.animIn:HookScript('OnFinished', OnFinish)
+			hooked[overlay] = true
 		end
+	end
   end
 })
