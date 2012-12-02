ccf888ceea552a17257a197ac6b0c84d48fa8b56
Jaliborc <contact@jaliborc.com>
2012-12-02 16:27:13 +0000

    Added french locale

---
+++ localization/fr.lua
@@ -0,0 +1,16 @@
+﻿--[[
+	OmniCC localization - French
+--]]
+
+if GetLocale() ~= 'frFR' then return end
+local L = OMNICC_LOCALS
+
+L.Pulse = "Pulsation"
+L.Shine = "Brillance"
+L.Alert = "Alerte"
+L.Activate = "Activation"
+
+--effect tooltip text
+L.ActivateTip = [[Imite l'effet par défaut qui apparaît quand un sort est activé.]]
+L.AlertTip = [[Affiche une pulsation au centre de l'écran lorsque le sort est prêt.]]
+L.PulseTip = [[Affiche une pulsation lorsque le sort est prêt.]]
