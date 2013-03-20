2fdee52a195e5d105b9ad85ba6896b41ffa5da1f
gbcat <tgbcat@163.com>
2013-03-20 23:23:42 +0800

    Update cn.lua
    Update this Simplified Chinese localization file based on the latest en.lua.

--- localization/cn.lua
+++ localization/cn.lua
@@ -6,18 +6,20 @@ if GetLocale() ~= 'zhCN' then return end
 
 local L = OMNICC_LOCALS
 
-L.Updated = "升级至 v%s"
+-- effect names
 L.None = NONE
-L.Pulse = "脉冲"
+L.Pulse = "搏动"
 L.Shine = "闪亮"
+L.Alert = "警报"
+L.Activate = "激活"
+L.Flare = "耀斑"
 
---slash command text
-L.SetEngine_Classic = '切换到经典计时器的引擎。此设置将在下次登录时生效.'
-L.SetEngine_Animation = '切换到经典计时器的动画引擎。此设置将在下次登录时生效.'
-L.UnknownEngineName = "未知的计时器引擎 '%s'"
-L.SetEngineUsage = '用法: /omnicc setengine <classic | animation>'
-L.Commands = '命令 (/omnicc or /occ):'
-L.Command_ShowOptionsMenu = '- config - 显示选项菜单'
-L.Command_SetTimerEngine = '- setengine <classic | animation> - 开关计时器的更新引擎'
-L.Command_ShowAddonVersion = '- version - 显示目前插件的版本'
-L.UnknownCommandName = "未知的命令 '%s'"
+--effect tooltips
+L.ActivateTip = [[模仿系统自带的，技能被“触发”的效果
+（如CTM时代术士燃放灵魂碎片之后灵魂之火按钮的效果）。]]
+L.AlertTip = [[结束冷却的图标在屏幕中央搏动。]]
+L.PulseTip = [[结束冷却的图标将发生搏动。]]
+
+-- other
+L.ConfigMissing = '%s无法被载入，因为该插件%s'
+L.Version = '你正在使用|cffFCF75EOmniCC 版本(%s)|r'
