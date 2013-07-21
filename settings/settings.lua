eae9ca240f725ca745c79a68d46f5343b6599bc5
Jaliborc <contact@jaliborc.com>
2013-07-21 18:46:06 +0100

    Adding "charging" defaults

--- settings/settings.lua
+++ settings/settings.lua
@@ -3,7 +3,56 @@
 		handles OmniCC saved variables
 --]]
 
-local Settings = 'OmniCC4Config'
+local SAVED_VAR = 'OmniCC4Config'
+local GROUP_DEFAULTS = {
+	enabled = true,
+	scaleText = true,
+	spiralOpacity = 1,
+	fontFace = STANDARD_TEXT_FONT,
+	fontSize = 18,
+	fontOutline = 'OUTLINE',
+	minDuration = 3,
+	minSize = 0.5,
+	effect = 'pulse',
+	minEffectDuration = 30,
+	tenthsDuration = 0,
+	mmSSDuration = 0,
+	xOff = 0,
+	yOff = 0,
+	anchor = 'CENTER',
+	styles = {
+		soon = {
+			r = 1, g = .1, b = .1, a = 1,
+			scale = 1.5
+		},
+		seconds = {
+			r = 1, g = 1, b = .1, a = 1,
+			scale = 1
+		},
+		minutes = {
+			r = 1, g = 1, b = 1, a = 1,
+			scale = 1
+		},
+		hours = {
+			r = .7, g = .7, b = .7, a = 1,
+			scale = .75
+		},
+		charging = {
+			r = .8, g = .8, b = .8, a = .7,
+			scale = .75
+		}
+	}
+}
+
+local DEFAULTS = {
+	engine = 'AniUpdater',
+	groups = {},
+	groupSettings = {base = GROUP_DEFAULTS}
+}
+
+
+--[[ Utilities ]]--
+
 local function CopyTable(target, source)
 	for k, v in pairs(source) do
 		if type(v) == 'table' then
@@ -25,10 +74,10 @@ end
 --[[ Startup ]]--
 
 function OmniCC:StartupSettings()
-	self.sets = _G[Settings] or self:GetDefaults()
+	self.sets = _G[SAVED_VAR] or DEFAULTS
 	self:UpgradeSettings()
 	
-	_G[Settings] = self.sets
+	_G[SAVED_VAR] = self.sets
 end
 
 function OmniCC:UpgradeSettings()
@@ -36,74 +85,32 @@ function OmniCC:UpgradeSettings()
 	local groups = self.sets.groupSettings
 
 	if version < 40000 then
-		self.sets = self:GetDefaults()
+		self.sets = DEFAULTS
 	elseif version < 50006 then
 		self.sets.engine = 'AniUpdater'
 		self.sets.updaterEngine = nil
 		
 		for _, group in pairs(groups) do
-			CopyTable(group, self:GetGroupDefaults())
+			CopyTable(group, GROUP_DEFAULTS)
 		end
-	elseif version < 50201 then
-		for _, group in pairs(groups) do
-			group.showCooldownModels = nil
-			group.spiralOpacity = 1
+	else
+		if version < 50201 then
+			for _, group in pairs(groups) do
+				group.showCooldownModels = nil
+				group.spiralOpacity = 1
+			end
+		end
+
+		if version < 50301 then
+			for _, group in pairs(groups) do
+				group.styles.charging = GROUP_DEFAULTS.styles.charging
+			end
 		end
 	end
 
 	self.sets.version = self:GetVersion()
 end
-	
 
---[[ Constants ]]--
-
-function OmniCC:GetDefaults()
-	return {
-		engine = 'AniUpdater',
-		groups = {},
-		groupSettings = {
-			base = self:GetGroupDefaults()
-		}
-	}
-end
-
-function OmniCC:GetGroupDefaults()
-	return {
-		enabled = true,
-		scaleText = true,
-		spiralOpacity = 1,
-		fontFace = STANDARD_TEXT_FONT,
-		fontSize = 18,
-		fontOutline = 'OUTLINE',
-		minDuration = 3,
-		minSize = 0.5,
-		effect = 'pulse',
-		minEffectDuration = 30,
-		tenthsDuration = 0,
-		mmSSDuration = 0,
-		xOff = 0,
-		yOff = 0,
-		anchor = 'CENTER',
-		styles = {
-			soon = {
-				r = 1, g = .1, b = .1, a = 1,
-				scale = 1.5
-			},
-			seconds = {
-				r = 1, g = 1, b = .1, a = 1,
-				scale = 1
-			},
-			minutes = {
-				r = 1, g = 1, b = 1, a = 1,
-				scale = 1
-			},
-			hours = {
-				r = 0.7, g = 0.7, b = 0.7, a = 1,
-				scale = 0.75
-			}
-		}
-	}
-end
 
 --[[ Version ]]--
 
