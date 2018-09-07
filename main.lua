cc42aabcb88078dae4bddfc393be9cc0a520926e
Jason Greer <tuller@tullamods.com>
2018-09-06 20:26:32 -0400

    move hooking code to main

--- main.lua
+++ main.lua
@@ -15,7 +15,8 @@ function Addon:Startup()
 		f:SetScript("OnShow", nil)
 	end)
 
-	self:RegisterEvent("VARIABLES_LOADED")
+	self:RegisterEvent("ADDON_LOADED")
+	self:RegisterEvent("PLAYER_ENTERING_WORLD")
 end
 
 function Addon:SetupCommands()
@@ -34,19 +35,100 @@ function Addon:SetupCommands()
 	end
 end
 
-function Addon:SetupEvents()
-	self:UnregisterEvent("VARIABLES_LOADED")
-	self:RegisterEvent("PLAYER_ENTERING_WORLD")
+function Addon:SetupHooks()
+	local Display = self.Display
+	local Timer = self.Timer
+
+    local blacklist = {}
+
+    local function deactivateDisplay(owner)
+		local display = Display:Get(owner)
+
+        if display then
+            display:Deactivate()
+        end
+    end
+
+    local function setBlacklisted(cooldown, blacklisted)
+        if blacklisted then
+            if not blacklist[cooldown] then
+				blacklist[cooldown] = true
+                deactivateDisplay(cooldown)
+            end
+        else
+            blacklist[cooldown] = nil
+        end
+    end
+
+    local function getKind(cooldown)
+        if cooldown.currentCooldownType == COOLDOWN_TYPE_LOSS_OF_CONTROL then
+            return "loc"
+        end
+
+        local parent = cooldown:GetParent()
+        if parent and parent.chargeCooldown == cooldown then
+            return "charge"
+        end
+
+        return nil
+	end
+
+	local Cooldown_MT = getmetatable(_G.ActionButton1Cooldown).__index
+
+	hooksecurefunc(Cooldown_MT, "Clear", function(cooldown)
+		deactivateDisplay(cooldown)
+	end)
+
+    hooksecurefunc(Cooldown_MT, "SetCooldown", function(cooldown, start, duration, modRate)
+        if cooldown.noCooldownCount or blacklist[cooldown] or cooldown:IsForbidden() then
+            return
+		end
+
+		local owner = cooldown:GetParent()
+		if not owner then
+			return
+		end
+
+		local enabled, minDuration
+		local settings = Addon:GetGroupSettingsFor(cooldown)
+        if settings then
+            enabled = settings.enabled
+			minDuration = settings.minDuration or 0
+        else
+            enabled = false
+			minDuration = 0
+		end
+
+		if enabled and (duration or 0) > minDuration then
+			local display = Display:Get(cooldown) or Display:Create(cooldown)
+
+			display:Activate(Timer:GetOrCreate(settings, start, duration, getKind(cooldown)))
+        else
+            deactivateDisplay(cooldown)
+        end
+    end)
+
+    -- hooksecurefunc(Cooldown_MT, "SetHideCountdownNumbers", function(cooldown, hide)
+    --     setBlacklisted(cooldown, hide)
+    -- end)
+
+    hooksecurefunc("CooldownFrame_SetDisplayAsPercentage", function(cooldown)
+        setBlacklisted(cooldown, true)
+    end)
 end
 
 -- Events
-function Addon:PLAYER_ENTERING_WORLD()
-	self.Timer:ForActive("Update")
+function Addon:ADDON_LOADED(event, addonName)
+	if addonName == AddonName then
+		self:UnregisterEvent(event)
+
+		self:StartupSettings()
+		self:SetupHooks()
+	end
 end
 
-function Addon:VARIABLES_LOADED()
-	self:StartupSettings()
-	self:SetupEvents()
+function Addon:PLAYER_ENTERING_WORLD()
+	self.Timer:ForActive("Update")
 end
 
 -- Utility
