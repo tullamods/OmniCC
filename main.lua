dbc2d8df277cc4815be4fd7349d98e63c9b3b77f
Jason Greer <tuller@tullamods.com>
2018-09-22 19:37:50 -0400

    move cooldown events stuff back to its own file

--- main.lua
+++ main.lua
@@ -1,3 +1,4 @@
+-- code to drive the addon
 local ADDON_NAME = ...
 local CONFIG_ADDON_NAME = ADDON_NAME .. "_Config"
 local L = _G.OMNICC_LOCALS
@@ -38,135 +39,13 @@ function Addon:SetupCommands()
 	end
 end
 
-function Addon:SetupHooks()
-	local Display = self.Display
-	local GetSpellCooldown = _G.GetSpellCooldown
-	local GCD_SPELL_ID = 61304
-
-	-- used to keep track of active cooldowns,
-	-- and the displays associated with them
-	local active = {}
-
-	-- used to keep track of cooldowns that we've hooked
-	local hooked = {}
-
-	local cooldown_ShowTimer, cooldown_HideTimer
-
-	local function cooldown_OnSetCooldown(cooldown, start, duration)
-        if cooldown.noCooldownCount or cooldown:IsForbidden() then
-            return
-		end
-
-		start = start or 0
-		if start == 0 then
-			cooldown_HideTimer(cooldown)
-			return
-		end
-
-		duration = duration or 0
-		if duration == 0 then
-			cooldown_HideTimer(cooldown)
-			return
-		end
-
-		-- stop timers replaced by global cooldown
-		local gcdStart, gcdDuration = GetSpellCooldown(GCD_SPELL_ID)
-        if start == gcdStart and duration == gcdDuration then
-			cooldown_HideTimer(cooldown)
-		else
-			cooldown_ShowTimer(cooldown, duration)
-		end
-	end
-
-	local function cooldown_OnSetCooldownDuration(cooldown, duration)
-        if cooldown.noCooldownCount or cooldown:IsForbidden() then
-            return
-		end
-
-		duration = duration or 0
-
-		if duration > 0 then
-			cooldown_ShowTimer(cooldown, duration)
-		else
-			cooldown_HideTimer(cooldown)
-		end
-	end
-
-	local function cooldown_OnSetDisplayAsPercentage(cooldown)
-		if not cooldown.noCooldownCount then
-			cooldown.noCooldownCount = true
-
-			cooldown_HideTimer(cooldown)
-		end
-	end
-
-	local function cooldown_OnShow(cooldown)
-		if not active[cooldown] then
-			local start, duration = cooldown:GetCooldownTimes()
-			if start and duration then
-				cooldown_OnSetCooldown(cooldown, start/1000, duration / 1000)
-			end
-		end
-	end
-
-	function cooldown_ShowTimer(cooldown, duration)
-		local minDuration
-
-		local settings = Addon:GetGroupSettingsFor(cooldown)
-        if settings and settings.enabled then
-			minDuration = settings.minDuration or 0
-        else
-			minDuration = math.huge
-		end
-
-		if (duration or 0) > minDuration then
-			if not hooked[cooldown] then
-				hooked[cooldown] = true
-				cooldown:HookScript("OnShow", cooldown_OnShow)
-				cooldown:HookScript("OnHide", cooldown_HideTimer)
-			end
-
-			-- handle a fun edge case of a cooldown with an already active
-			-- display that now belongs to a different parent object
-			local oldDisplay = active[cooldown]
-			local newDisplay = Display:GetOrCreate(cooldown:GetParent() or cooldown)
-
-			if oldDisplay and oldDisplay ~= newDisplay then
-				oldDisplay:HideCooldownText(cooldown)
-			end
-
-			if newDisplay then
-				newDisplay:ShowCooldownText(cooldown)
-			end
-
-			active[cooldown] = newDisplay
-		else
-			cooldown_HideTimer(cooldown)
-		end
-	end
-
-	function cooldown_HideTimer(cooldown)
-		local display = active[cooldown]
-
-		if display then
-			display:HideCooldownText(cooldown)
-			active[cooldown] = nil
-		end
-	end
-
-	local Cooldown_MT = getmetatable(_G.ActionButton1Cooldown).__index
-	hooksecurefunc(Cooldown_MT, "SetCooldown", cooldown_OnSetCooldown)
-	hooksecurefunc(Cooldown_MT, "SetCooldownDuration", cooldown_OnSetCooldownDuration)
-	hooksecurefunc("CooldownFrame_SetDisplayAsPercentage", cooldown_OnSetDisplayAsPercentage)
-end
-
 -- Events
 function Addon:ADDON_LOADED(event, ...)
 	if ADDON_NAME ~= ... then return end
 
 	self:UnregisterEvent(event)
 	self:StartupSettings()
-	self:SetupHooks()
+	self:WatchCooldowns()
 end
 
 function Addon:PLAYER_ENTERING_WORLD()
