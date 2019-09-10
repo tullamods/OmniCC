0f5065b4b60109c5917d2b363118676d3f4dd064
Pgmbru <pgmbru@hotmail.fr>
2019-09-10 21:57:55 +0200

    Update frFR.lua
    Add missing script lines

--- main/localization/frFR.lua
+++ main/localization/frFR.lua
@@ -1,15 +1,28 @@
-﻿local AddonName = ...
+local AddonName = ...
 local L = LibStub("AceLocale-3.0"):NewLocale(AddonName, "frFR")
 if not L then return end
 
+-- timer formats
+L.TenthsFormat = "%0.1f"
+L.SecondsFormat = "%d"
+L.MMSSFormat = "%d:%02d"
+L.MinuteFormat = "%dm"
+L.HourFormat = "%dh"
+L.DayFormat = "%dd"
 
--- effect names
+-- Noms des effects
+L.None = NONE
 L.Pulse = "Pulsation"
-L.Shine = "Brillance"
+L.Shine = "Éclat"
 L.Alert = "Alerte"
 L.Activate = "Activation"
+L.Flare = "Éclater"
 
--- effect tooltip text
-L.ActivateTip = [[Imite l'effet par défaut qui apparaît quand un sort est activé.]]
+-- Info-bulles d'effet
+L.ActivateTip = [[Applique l'effet déclencheur de capacité à l'icône de temps de recharge.]]
 L.AlertTip = [[Affiche une pulsation au centre de l'écran lorsque le sort est prêt.]]
 L.PulseTip = [[Affiche une pulsation lorsque le sort est prêt.]]
+
+-- other
+L.ConfigMissing = "%s n'a pas pu être chargé car l'addon est %s"
+L.Version = "Vous utilisez |cffFCF75EOmniCC Version (%s)|r"
