a1365a3b2ef6bdd2ef4bd4804a5cd460f865e7e5
Jason Greer <tuller@tullamods.com>
2025-09-07 10:21:01 -0400

    cleanup animation pools

--- OmniCC/effects/flare.lua
+++ OmniCC/effects/flare.lua
@@ -2,107 +2,95 @@
 local ADDON, Addon = ...
 local L = LibStub("AceLocale-3.0"):GetLocale(ADDON)
 
-local SHINE_TEXTURE = ([[Interface\Addons\%s\media\flare]]):format(ADDON)
-local SHINE_DURATION = 0.75
-local SHINE_SCALE = 5
+local FLARE_TEXTURE = ([[Interface\Addons\%s\media\flare]]):format(ADDON)
+local FLARE_DURATION = 0.75
+local FLARE_SCALE = 5
 
 local FlareEffect = Addon.FX:Create("flare", L.Flare)
-local FlarePool
-do
-	local function shineAnimation_OnFinished(self)
-		local parent = self:GetParent()
-		if parent:IsShown() then
-			parent:Hide()
-		end
+
+local unused = {}
+
+local function onFlareAnimationFinished(self)
+	local parent = self:GetParent()
+	if parent:IsShown() then
+		parent:Hide()
 	end
+end
 
-	local function shineAnimation_Create(parent)
-		local group = parent:CreateAnimationGroup()
-		group:SetScript('OnFinished', shineAnimation_OnFinished)
-		group:SetLooping('NONE')
+local function createFlareAnimation(parent)
+	local animation = parent:CreateAnimationGroup()
+	animation:SetScript('OnFinished', onFlareAnimationFinished)
+	animation:SetLooping('NONE')
 
-		local initiate = group:CreateAnimation('Alpha')
-		initiate:SetFromAlpha(1)
-		initiate:SetDuration(0)
-		initiate:SetToAlpha(0)
-		initiate:SetOrder(0)
+	local init = animation:CreateAnimation('Alpha')
+	init:SetFromAlpha(1)
+	init:SetDuration(0)
+	init:SetToAlpha(0)
+	init:SetOrder(0)
 
-		local grow = group:CreateAnimation('Scale')
-		grow:SetOrigin('CENTER', 0, 0)
-		grow:SetScale(SHINE_SCALE, SHINE_SCALE)
-		grow:SetDuration(SHINE_DURATION / 2)
-		grow:SetOrder(1)
+	local grow = animation:CreateAnimation('Scale')
+	grow:SetOrigin('CENTER', 0, 0)
+	grow:SetScale(FLARE_SCALE, FLARE_SCALE)
+	grow:SetDuration(FLARE_DURATION / 2)
+	grow:SetOrder(1)
 
-		local brighten = group:CreateAnimation('Alpha')
-		brighten:SetDuration(SHINE_DURATION / 2)
-		brighten:SetFromAlpha(0)
-		brighten:SetToAlpha(1)
-		brighten:SetOrder(1)
+	local brighten = animation:CreateAnimation('Alpha')
+	brighten:SetDuration(FLARE_DURATION / 2)
+	brighten:SetFromAlpha(0)
+	brighten:SetToAlpha(1)
+	brighten:SetOrder(1)
 
-		local shrink = group:CreateAnimation('Scale')
-		shrink:SetOrigin('CENTER', 0, 0)
-		shrink:SetScale(1/SHINE_SCALE, 1/SHINE_SCALE)
-		shrink:SetDuration(SHINE_DURATION / 2)
-		shrink:SetOrder(2)
+	local shrink = animation:CreateAnimation('Scale')
+	shrink:SetOrigin('CENTER', 0, 0)
+	shrink:SetScale(1/FLARE_SCALE, 1/FLARE_SCALE)
+	shrink:SetDuration(FLARE_DURATION / 2)
+	shrink:SetOrder(2)
 
-		local fade = group:CreateAnimation('Alpha')
-		fade:SetDuration(SHINE_DURATION / 2)
-		fade:SetFromAlpha(1)
-		fade:SetToAlpha(0)
-		fade:SetOrder(2)
+	local fade = animation:CreateAnimation('Alpha')
+	fade:SetDuration(FLARE_DURATION / 2)
+	fade:SetFromAlpha(1)
+	fade:SetToAlpha(0)
+	fade:SetOrder(2)
 
-		return group
+	return animation
+end
+
+local function onFlareFrameHidden(self)
+	if not unused[self] then
+		unused[self] = true
+		self:StopAnimating()
+		self:Hide()
 	end
+end
 
-	local function shine_OnHide(self)
-		if not self.released then
-			FlarePool:Release(self)
-		end
-	end
+local function createFlareFrame()
+	local frame = CreateFrame('Frame')
+	frame:Hide()
+	frame:SetScript('OnHide', onFlareFrameHidden)
+	frame:SetToplevel(true)
 
-	local function pool_OnCreate(self)
-		local shine = CreateFrame('Frame')
-		shine:Hide()
-		shine:SetScript('OnHide', shine_OnHide)
-		shine:SetToplevel(true)
+	local icon = frame:CreateTexture(nil, 'OVERLAY')
+	icon:SetPoint('CENTER')
+	icon:SetBlendMode('ADD')
+	icon:SetAllPoints(icon:GetParent())
+	icon:SetTexture(FLARE_TEXTURE)
 
-		local icon = shine:CreateTexture(nil, 'OVERLAY')
-		icon:SetPoint('CENTER')
-		icon:SetBlendMode('ADD')
-		icon:SetAllPoints(icon:GetParent())
-		icon:SetTexture(SHINE_TEXTURE)
+	frame.animation = createFlareAnimation(frame)
 
-		shine.animation = shineAnimation_Create(shine)
-
-		return shine
-	end
-
-	local function pool_OnRelease(self, shine)
-		self.released = true
-
-		if shine.animation:IsPlaying() then
-			shine.animation:Finish()
-		end
-
-		shine:Hide()
-		shine:SetParent(nil)
-	end
-
-	FlarePool = CreateObjectPool(pool_OnCreate, pool_OnRelease)
+	return frame
 end
 
 function FlareEffect:Run(cooldown)
 	local owner = cooldown:GetParent() or cooldown
 
 	if owner and owner:IsVisible() then
-		local shine = FlarePool:Acquire()
+		local shine = next(unused) or createFlareFrame()
+		unused[shine] = nil
 
 		shine:SetParent(owner)
 		shine:ClearAllPoints()
 		shine:SetAllPoints(cooldown)
 		shine:Show()
-
-		shine.animation:Stop()
 		shine.animation:Play()
 	end
 end
--- OmniCC/effects/shine.lua
+++ OmniCC/effects/shine.lua
@@ -7,99 +7,90 @@ local SHINE_DURATION = 0.75
 local SHINE_SCALE = 5
 
 local ShineEffect = Addon.FX:Create("shine", L.Shine)
-local ShinePool
-do
-	local function shineAnimation_OnFinished(self)
-		local parent = self:GetParent()
 
-		if parent:IsShown() then
-			parent:Hide()
-		end
+local unused = {}
+
+local function onShineAnimationFinished(self)
+	local parent = self:GetParent()
+	if parent:IsShown() then
+		parent:Hide()
 	end
+end
 
-	local function shineAnimation_Create(parent)
-		local group = parent:CreateAnimationGroup()
-		group:SetScript('OnFinished', shineAnimation_OnFinished)
-		group:SetLooping('NONE')
+local function createShineAnimation(parent)
+	local group = parent:CreateAnimationGroup()
+	group:SetScript('OnFinished', onShineAnimationFinished)
+	group:SetLooping('NONE')
 
-		local initiate = group:CreateAnimation('Alpha')
-		initiate:SetFromAlpha(1)
-		initiate:SetDuration(0)
-		initiate:SetToAlpha(0)
-		initiate:SetOrder(0)
+	local init = group:CreateAnimation('Alpha')
+	init:SetFromAlpha(1)
+	init:SetDuration(0)
+	init:SetToAlpha(0)
+	init:SetOrder(0)
 
-		local grow = group:CreateAnimation('Scale')
-		grow:SetOrigin('CENTER', 0, 0)
-		grow:SetScale(SHINE_SCALE, SHINE_SCALE)
-		grow:SetDuration(SHINE_DURATION / 2)
-		grow:SetOrder(1)
+	local grow = group:CreateAnimation('Scale')
+	grow:SetOrigin('CENTER', 0, 0)
+	grow:SetScale(SHINE_SCALE, SHINE_SCALE)
+	grow:SetDuration(SHINE_DURATION / 2)
+	grow:SetOrder(1)
 
-		local brighten = group:CreateAnimation('Alpha')
-		brighten:SetDuration(SHINE_DURATION / 2)
-		brighten:SetFromAlpha(0)
-		brighten:SetToAlpha(1)
-		brighten:SetOrder(1)
+	local brighten = group:CreateAnimation('Alpha')
+	brighten:SetDuration(SHINE_DURATION / 2)
+	brighten:SetFromAlpha(0)
+	brighten:SetToAlpha(1)
+	brighten:SetOrder(1)
 
-		local shrink = group:CreateAnimation('Scale')
-		shrink:SetOrigin('CENTER', 0, 0)
-		shrink:SetScale(1/SHINE_SCALE, 1/SHINE_SCALE)
-		shrink:SetDuration(SHINE_DURATION / 2)
-		shrink:SetOrder(2)
+	local shrink = group:CreateAnimation('Scale')
+	shrink:SetOrigin('CENTER', 0, 0)
+	shrink:SetScale(1 / SHINE_SCALE, 1 / SHINE_SCALE)
+	shrink:SetDuration(SHINE_DURATION / 2)
+	shrink:SetOrder(2)
 
-		local fade = group:CreateAnimation('Alpha')
-		fade:SetDuration(SHINE_DURATION / 2)
-		fade:SetFromAlpha(1)
-		fade:SetToAlpha(0)
-		fade:SetOrder(2)
+	local fade = group:CreateAnimation('Alpha')
+	fade:SetDuration(SHINE_DURATION / 2)
+	fade:SetFromAlpha(1)
+	fade:SetToAlpha(0)
+	fade:SetOrder(2)
 
-		return group
+	return group
+end
+
+local function onShineFrameHidden(self)
+	if not unused[self] then
+		unused[self] = true
+		self:StopAnimating()
+		self:Hide()
 	end
+end
 
-	local function shine_OnHide(self)
-		ShinePool:Release(self)
-	end
+local function createShineFrame()
+	local frame = CreateFrame('Frame')
+	frame:Hide()
+	frame:SetScript('OnHide', onShineFrameHidden)
+	frame:SetToplevel(true)
 
-	local function pool_OnCreate(self)
-		local shine = CreateFrame('Frame')
-		shine:Hide()
-		shine:SetScript('OnHide', shine_OnHide)
-		shine:SetToplevel(true)
+	local icon = frame:CreateTexture(nil, 'OVERLAY')
+	icon:SetPoint('CENTER')
+	icon:SetBlendMode('ADD')
+	icon:SetAllPoints(icon:GetParent())
+	icon:SetTexture(SHINE_TEXTURE)
 
-		local icon = shine:CreateTexture(nil, 'OVERLAY')
-		icon:SetPoint('CENTER')
-		icon:SetBlendMode('ADD')
-		icon:SetAllPoints(icon:GetParent())
-		icon:SetTexture(SHINE_TEXTURE)
+	frame.animation = createShineAnimation(frame)
 
-		shine.animation = shineAnimation_Create(shine)
-
-		return shine
-	end
-
-	local function pool_OnRelease(self, shine)
-		if shine.animation:IsPlaying() then
-			shine.animation:Finish()
-		end
-
-		shine:Hide()
-		shine:SetParent(nil)
-	end
-
-	ShinePool = CreateObjectPool(pool_OnCreate, pool_OnRelease)
+	return frame
 end
 
 function ShineEffect:Run(cooldown)
 	local owner = cooldown:GetParent() or cooldown
 
 	if owner and owner:IsVisible() then
-		local shine = ShinePool:Acquire()
+		local shine = next(unused) or createShineFrame()
+		unused[shine] = nil
 
 		shine:SetParent(owner)
 		shine:ClearAllPoints()
 		shine:SetAllPoints(cooldown)
 		shine:Show()
-
-		shine.animation:Stop()
 		shine.animation:Play()
 	end
 end
